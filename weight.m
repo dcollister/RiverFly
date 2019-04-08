d42c066aa52b41da753fdb9dded1aec10d29abd4
dcollister <44823203+dcollister@users.noreply.github.com>
2019-04-08 07:18:41 -0700

    Add files via upload

--- weight.m
+++ weight.m
@@ -3,7 +3,7 @@ function[riverWeight,rowsTop,columnsTop]=weight(river)
 riverWeight=cell(size(river,1),size(river,2));
 for i=1:size(riverWeight,1)
     for j=1:size(riverWeight,2)
-        riverWeight{i,j}=zeros(2,9);
+        riverWeight{i,j}=zeros(4,9);
     end
 end
 
@@ -13,10 +13,12 @@ for i=1:size(rowsTop,1)
     for jj=1:3
         for ii=1:3
             riverWeight{rowsTop(i),columnsTop(i)}(1,(ii+3*(jj-1)))=river(rowsTop(i)-2+ii,columnsTop(i)-2+jj)/levelWeight;
+            riverWeight{rowsTop(i),columnsTop(i)}(3,(ii+3*(jj-1)))=river(rowsTop(i)-2+ii,columnsTop(i)-2+jj)/levelWeight;
         end
     end
     
     for iii=1:9
        riverWeight{rowsTop(i),columnsTop(i)}(2,iii)=sum(riverWeight{rowsTop(i),columnsTop(i)}(1,1:iii));
+       riverWeight{rowsTop(i),columnsTop(i)}(4,iii)=sum(riverWeight{rowsTop(i),columnsTop(i)}(3,1:iii));
     end
 end
